ec2aea499a737b86a3159a2fd0ebc9367f6508b4
FrancisSimonA <francissimonaquino@gmail.com>
2022-06-22 14:06:20 +0800

    added crud functionalities to database (GET, PUT)

--- backend/server.js
+++ backend/server.js
@@ -1,77 +1,24 @@
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const express = require("express");
-const debug = require("debug");
-const http = require("http");
-const cors = require("cors");
+import express from "express";
+import dotenv from "dotenv";
+import connectDb from "./config/mongodb.js";
+import ImportData from "./DataImport.js";
+import productRoute from "./routes/ProductRoutes.js";
+import userRoute from "./routes/UserRoutes.js";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
 
-const router = require("./routes/function_router");
+dotenv.config();
+connectDb();
 const app = express();
 
-// Database Credentials to allow the connection.
-const dbcreds = require("../credentials");
-app.use(express.urlencoded({ extended: true}));
+// API
+app.use("/api/import", ImportData);
+app.use("/api/products", productRoute);
+app.use("/api/users", userRoute);
 
-// Forming the connection to the database.
-const dbURI = `mongodb+srv://${dbcreds.clusterName}:${dbcreds.databasePassword}@cluster0.psmwl.mongodb.net/${dbcreds.databaseName}?retryWrites=true&w=majority`;
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => { console.log("Connected to database!"); })
-.catch(() => { console.log("Connection failed!"); });
+// ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
 
-// Use CORS for complying to the policy.
-app.use( cors({ origin: "http://localhost:3000", methods: "GET,POST,PUT,DELETE", credentials: true, exposedHeaders: ['set-cookie'] }) );
+const PORT = process.env.PORT || 1000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const normalizePort = val => {
-    var port = parseInt(val, 10);
-  
-    // named pipe and port.
-    if (isNaN(port)) return val;
-    if (port >= 0) return port;
-  
-    return false;
-};
-  
-  // Setting up Callbacks.
-const onError = error => {
-    if (error.syscall !== "listen") throw error;
-        const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-        switch (error.code) {
-            case "EACCES":
-            console.error(bind + " requires elevated privileges");
-            process.exit(1);
-            break;
-            case "EADDRINUSE":
-            console.error(bind + " is already in use");
-            process.exit(1);
-            break;
-            default:
-            throw error;
-    }
-}
-
-const onListening = () => {
-    const addr = server.address();
-    const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
-    debug("Listening on " + bind);
-}
-
-
-// Starting the Server.
-const onServerStart = () => {
-    console.log('Ultimate Ecommerce App server is running at PORT: ', app.get('port'));
-}
-
-// Set the PORT.
-const port = normalizePort(process.env.PORT || "5000");
-app.set("port", port);
-
-// Start the Server.
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port, onServerStart);
-
-app.use("/", router);
+app.listen(PORT, console.log(`server running on port ${PORT}...`));
